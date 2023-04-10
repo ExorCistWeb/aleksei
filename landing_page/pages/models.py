840f18ddbe234cc1e3fbff62e0b68e449b0c9829
Chyngyz Git <cbaibosunov@gmai.com>
2023-04-10 17:07:12 +0700

    1

--- landing_page/pages/models.py
+++ landing_page/pages/models.py
@@ -4,9 +4,12 @@ from django.db import models
 # Create your models here.
 class image(models.Model):
     data = models.DateTimeField(null=True)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to='images/')
     subject = models.TextField(max_length=500)
     text = models.TextField(max_length=500)
+
+    def __str__(self) -> str:
+        return str(self.image)
 
     class Meta:
         verbose_name = ('Слайдер')
@@ -18,6 +21,10 @@ class Individual_consultation(models.Model):
         verbose_name=('Цена'), 
         default=0
     )
+
+    def __str__(self) -> str:
+        return str(self.time)
+
     class Meta:
         verbose_name = ('Индивидуальная консультация')
         verbose_name_plural = ('Индивидуальная консультация')
@@ -28,6 +35,10 @@ class Family_consultation(models.Model):
         verbose_name=('Цена'), 
         default=0
     )
+
+    def __str__(self) -> str:
+        return str(self.time )
+
     class Meta:
         verbose_name = ('Семейная консультация')
         verbose_name_plural = ('Семейная консультация')
@@ -46,6 +57,10 @@ class Consultation_subscription5(models.Model):
         verbose_name=('Цена по скидке?'), 
         default=False
     )
+
+    def __str__(self) -> str:
+        return str(self.time )
+
     class Meta:
         verbose_name = ('Абонемент 5 консультаций')
         verbose_name_plural = ('Абонемент 5 консультаций')
@@ -66,6 +81,10 @@ class Consultation_subscription10(models.Model):
         verbose_name=('Цена по скидке?'), 
         default=False
     )
+
+    def __str__(self) -> str:
+        return str(self.time )
+    
     class Meta:
         verbose_name = ('Абонемент 10 консультаций')
         verbose_name_plural = ('Абонемент 10 консультаций')
@@ -84,6 +103,10 @@ class Consultation_subscription15(models.Model):
         verbose_name=('Цена по скидке?'), 
         default=False
     )
+
+    def __str__(self) -> str:
+        return str(self.time )
+
     class Meta:
         verbose_name = ('Абонемент 15 консультаций')
         verbose_name_plural = ('Абонемент 15 консультаций')
