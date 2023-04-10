8f1fa2fe9ecab884db4d4ced19bd88cfabad90fb
Chyngyz Git <cbaibosunov@gmai.com>
2023-04-10 15:55:24 +0700

    1

--- landing_page/pages/admin.py
+++ landing_page/pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.image) 
+admin.site.register(models.Individual_consultation) 
+admin.site.register(models.Family_consultation) 
+admin.site.register(models.Consultation_subscription5) 
+admin.site.register(models.Consultation_subscription10) 
+admin.site.register(models.Consultation_subscription15) 
--- landing_page/pages/models.py
+++ landing_page/pages/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class image(models.Model):
+    data = models.DateTimeField(null=True,verbose_name='Название товара',)
+    image = models.ImageField(blank=True, null=True,verbose_name='Название товара',)
+    subject = models.TextField(max_length=500, verbose_name='Название товара',)
+    text = models.TextField(max_length=500, verbose_name='Название товара',)
+
+class Individual_consultation(models.Model):
+    time = models.DecimalField(verbose_name='Название товара',)
+    price = models.CharField(max_length=10,verbose_name='Название товара',)
+
+class Family_consultation(models.Model):
+    time = models.DecimalField(verbose_name='Название товара',)
+    price = models.CharField(max_length=10, verbose_name='Название товара',)
+
+class Consultation_subscription5(models.Model):
+    time = models.DecimalField(
+        verbose_name='Название товара',
+        )
+    price = models.CharField(
+        max_length=10,
+        verbose_name='Название товара',
+        )
+    discount = models.IntegerField(
+        verbose_name=('Цена со скидкой'), 
+        default=0
+    )
+
+class Consultation_subscription10(models.Model):
+    time = models.DecimalField(
+        verbose_name='Название товара',
+        )
+    price = models.CharField(
+        max_length=10,verbose_name='Название товара',
+        )
+    discount = models.IntegerField(
+        verbose_name=('Цена со скидкой'), 
+        default=0
+    )
+
+class Consultation_subscription15(models.Model):
+    time = models.DecimalField(
+        verbose_name='Название товара',
+        )
+    price = models.CharField(
+        max_length=10,verbose_name='Название товара',
+        )
+    discount = models.IntegerField(
+        verbose_name=('Цена со скидкой'), 
+        default=0
+    )
+    many = models.IntegerField(
+        verbose_name=('Цена со скидкой'), 
+        default=0,
+    )
